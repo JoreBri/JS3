142a0b360864c8f8450298bffd0f11dad5cb3491
JoreBri <jorebrizuela.jb@gmail.com>
2024-04-21 16:38:48 -0300

    mejoras

--- js/main.js
+++ js/main.js
@@ -1,12 +1,33 @@
-document.getElementById('enviar').addEventListener('click', function() {
-    var nombre = document.getElementById('Nombre').value;
-    var mensajeBienvenida = document.getElementById('mensajeBienvenida');
-    mensajeBienvenida.textContent = 'Hola ' + nombre + ', Bienvenido a Uñicornio';
-    
-    // Ocultar el cuadro de nombre
-    var cuadroNombre = document.getElementById('pedirNombre');
-    cuadroNombre.style.display = 'none';
-});
+document.addEventListener("DOMContentLoaded", function() {
+    const modal = document.getElementById("modal");
+    const nombreDelCliente = document.querySelector(".nombreDelCliente");
+    const nombreUsuario = localStorage.getItem("nombreUsuario");
+
+    if (nombreUsuario) {
+        nombreDelCliente.textContent = `Hola ${nombreUsuario}, te doy la bienvenida a Uñicornio.`;
+    } else {
+        modal.style.display = "block";
+    }
+})
+
+function guardarNombre() {
+    const nombre = document.getElementById("inputNombre").value;
+    const nombreDelCliente = document.querySelector(".nombreDelCliente");
+
+    if (nombre) {
+        localStorage.setItem("nombreUsuario", nombre);
+        console.log("nombre guardado en storage", nombre);
+        nombreDelCliente.textContent = `Hola ${nombreUsuario}, te doy la bienvenida a Uñicornio.`;
+        cerrarModal();
+    } else {
+        console.log("Ingresa tu nombre");
+    }
+}
+
+function cerrarModal() {
+    const modal = document.getElementById("modal");
+    modal.style.display = "none";
+}
 
 const carrito = [];
 
@@ -63,79 +84,92 @@ const precioTotal = document.querySelector ("#precioTotal");
 servicios.forEach((servicio) => {
     const div = document.createElement("div");
     div.classList.add("servicio");
-    div.innerHTML= `<img class="servicioImg" src="${servicio.img}" alt= "${servicio.titulo}">
-    <p class= "servicioTexto"> ${servicio.texto}.</p>
-    <h3>${servicio.titulo}</h3>
-    <p>$${servicio.precio}</p>
-    `;
-    
+    div.innerHTML = `<h3>${servicio.titulo}</h3>
+    <img class="servicioImg" src="${servicio.img}" alt="${servicio.titulo}">
+    <p class="servicioTexto">${servicio.texto}.</p>
+    <p>$${servicio.precio}</p>`;
+
     const btn = document.createElement("button");
     btn.classList.add("servicioBtn");
     btn.innerText = "Adquirir Servicio";
 
-    btn.addEventListener ("click", () => {
+    btn.addEventListener("click", () => {
         agregarAlCarrito(servicio);
-    })
+    });
 
-    div.append (btn);
+    div.append(btn);
     contenedorServicios.append(div);
-})
+});
 
 function actualizarCarrito() {
     if (carrito.length === 0) {
         carritoVacio.classList.remove("dNone");
-    carritoServicios.classList.add("dNone");
-} else {
+        carritoServicios.classList.add("dNone");
+    } else {
         carritoVacio.classList.add("dNone");
-    carritoServicios.classList.remove("dNone");
-    carritoServicios.innerHTML = "";
-            carrito.forEach(servicio => {
-                    const div = document.createElement("div");
-                div.classList.add("carritoServicio");
-                div.innerHTML = `
-                    <h3>${servicio.titulo}</h3>
-                    <p>$${servicio.precio}</p>
-                    <p>Cant: ${servicio.cantidad}</p>
-                     <p>Subt: $${servicio.cantidad * servicio.precio}</p>
-                `;
+        carritoServicios.classList.remove("dNone");
+        carritoServicios.innerHTML = "";
+        carrito.forEach(servicio => {
+            const div = document.createElement("div");
+            div.classList.add("carritoServicio");
+            div.innerHTML = `
+                <h3>${servicio.titulo}</h3>
+                <p>$${servicio.precio}</p>
+                <p>Cant: ${servicio.cantidad}</p>
+                <p>Subt: $${servicio.cantidad * servicio.precio}</p>
+            `;
 
-    const btn = document.createElement("button");
-                    btn.classList.add("carrito-producto-btn");
-                    btn.innerText = "✖️";
+            const btn = document.createElement("button");
+            btn.classList.add("carritoProductoBtn");
+            btn.innerText = "✖️";
 
-        btn.addEventListener("click", () => {
-                            borrarDelCarrito(servicio);
-                    })
+            btn.addEventListener("click", () => {
+                borrarDelCarrito(servicio);
+            });
 
-        div.append(btn);
+            div.append(btn);
+            carritoServicios.append(div);
+        });
+    }
+    actualizarTotal();
+}
 
-        carritoServicios.append(div)
-                })
-            }
-            actualizarTotal();
-        }
-
-
-// Se va a llamar cuando agregue un producto al carrito
-// Primero chequea si está el elemento, y si está le suma 1 a cantidad; si no está, lo pushea
 const agregarAlCarrito = (servicio) => {
     const itemEncontrado = carrito.find(item => item.titulo === servicio.titulo);
-if (itemEncontrado) {
-    itemEncontrado.cantidad++;
-} else {
-    carrito.push({...servicio, cantidad: 1});
-}
+    if (itemEncontrado) {
+        itemEncontrado.cantidad++;
+    } else {
+        carrito.push({ ...servicio, cantidad: 1 });
+    }
 
-actualizarCarrito();
-}
+    actualizarCarrito();
+};
 
 const borrarDelCarrito = (servicio) => {
-const prodIndex = carrito.findIndex(item => item.titulo === servicio.titulo);
-carrito.splice(prodIndex, 1);
-actualizarCarrito();
-}
+    const prodIndex = carrito.findIndex(item => item.titulo === servicio.titulo);
+    carrito.splice(prodIndex, 1);
+    actualizarCarrito();
+};
 
 const actualizarTotal = () => {
- const total = carrito.reduce((acc, serv) => acc + (serv.precio * serv.cantidad), 0);
-carritoTotal.innerText = `$${total}`;
+    const total = carrito.reduce((acc, serv) => acc + (serv.precio * serv.cantidad), 0);
+    precioTotal.innerText = `$${total}`;
+};
+
+botonCompra.addEventListener("click", finalizarCompra);
+function finalizarCompra() {
+    // Mostrar mensaje de confirmación de compra
+    alert("¡Gracias por tu compra!");
+
+    // Limpiar el carrito
+    carrito.length = 0;
+    actualizarCarrito(); // Actualizar la interfaz del carrito
+
+    // Otra opción para limpiar el carrito es reiniciar el array:
+    // carrito = [];
+
+    // Ocultar el contenedor del carrito
+    contenedorCarrito.classList.add("dNone");
 }
+// Llamar a la función para mostrar los servicios cuando se carga la página
+mostrarServicios();
